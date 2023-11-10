8b3812ce3a0e6b2ae49c5617b3186459f4efed16
Anjalib01 <anjalibhadekar2@gmail.com>
2023-11-10 10:44:05 +0530

    First Commit

---
+++ src/main/java/com/tyss/textilestockmanagement/TextileStockManagementApplication.java
@@ -0,0 +1,13 @@
+package com.tyss.textilestockmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextileStockManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextileStockManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tyss/textilestockmanagement/TextileStockManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tyss.textilestockmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextileStockManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
